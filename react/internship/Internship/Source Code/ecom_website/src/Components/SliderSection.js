64f78766b826a3f982847983fe1df11be4a7dd92
Dipan Nama <dipannama91@gmail.com>
2022-11-28 22:46:57 +0530

    something special

--- react/internship/Internship/Source Code/ecom_website/src/Components/SliderSection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const SliderSection = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- react/internship/Internship/Source Code/ecom_website/src/Pages/TermsAndConditioins.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const TermsAndConditions = () => {
-    return (
-        <div>TermsAndConditions</div>
-    )
-}
