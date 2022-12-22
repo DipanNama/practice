24c4b0eb9e89e9eadf7d2311ac45d2d52ba7825c
Dipan Nama <dipannama91@gmail.com>
2022-12-22 19:06:32 +0530

    something very important is hepenning here

--- react/projects/project_01/todoList_V02/src/components/Header.js
+++ react/projects/project_01/todoList_V02/src/components/Header.js
@@ -3,7 +3,11 @@ import '../App.css';
 export const Header = () => {
   return (
     <div>
-      <div className="App-header" id="drag" />
+      <div className="App-header" id="drag">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
     </div>
   )
 }
